0878e0e699915e2619edd8dfba2fcf8bab839410
PenguinKeeper <penguinkeeper@keemail.me>
2015-09-30 15:59:01 +0100

    - Added missing files from previous commit!

---
+++ app/src/main/java/com/blindcarboncopy/emotionalrobot/MyApplication.java
@@ -0,0 +1,14 @@
+package com.blindcarboncopy.emotionalrobot;
+
+import android.app.Application;
+
+/**
+ * Custom Application to enable the start of a Service on App launch.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/blindcarboncopy/emotionalrobot/data/IFeedProvider.java
@@ -0,0 +1,18 @@
+package com.blindcarboncopy.emotionalrobot.data;
+
+import com.blindcarboncopy.emotionalrobot.model.NodeRedMessage;
+
+import java.util.List;
+
+/**
+ * Defines behaviour for a feed provider.
+ */
+public interface IFeedProvider {
+    /**
+     * Gets an un-filtered message feed list from the content provider.
+     *
+     * @param showOnlyHappy If true, only happy messages are included in the returned message list.
+     * @return A list of messages.
+     */
+    public List<NodeRedMessage> getMessages(boolean showOnlyHappy);
+}
